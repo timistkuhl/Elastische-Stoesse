e6b8f360b2e8b5ba8a4bed83a9e035aeecd9ef8e
unknown <timletelgte11@gmail.com>
2026-06-16 17:45:29 +0200

    Added more parameters to ball object

--- ball.py
+++ ball.py
@@ -1,6 +1,9 @@
 import numpy as np
 
+
 class Ball:
-    def __init__(self: Ball, position: np.array, speed: np.array):
+    def __init__(self: Ball, position: np.array, speed: np.array, radius: float, mass: float):
         self.position = position
         self.speed = speed
+        self.radius = radius
+        self.mass = mass
