5d0c5bce3350b6b2497cc1881cf3d2fedb4fc151
Johannes Harmeling <johannes.harmeling@icloud.com>
2026-06-30 15:34:58 +0200

    n

--- setups/pi.py
+++ setups/pi.py
@@ -7,7 +7,7 @@ def getSetup() -> settings:
     objects = []
     objects.append(wall.Wall(np.array([800, 600], dtype='f'), np.array([800, 400], dtype='f')))
     
-    objects.append(ball.Ball(np.array([200, 500], dtype='f'), np.array([0.1, 0], dtype='f'), 40, 0.01))
+    objects.append(ball.Ball(np.array([200, 500], dtype='f'), np.array([0.1, 0], dtype='f'), 40, 0.1))
     objects.append(ball.Ball(np.array([500, 500], dtype='f'), np.array([0, 0], dtype='f'), 40, 0))
 
     return settings.Settings(objects, counting = True)
